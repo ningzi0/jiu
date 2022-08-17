c5a1511f5d6032a6d7ff3afadb19422da62dc09e
Your Name <you@example.com>
2022-08-17 14:44:16 +0800

    积分明细

---
+++ pages/integralDel/integralDel.js
@@ -0,0 +1,80 @@
+// pages/integralDel/integralDel.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    currentId: '1',
+    section: [{
+      name: '发放',
+      typeId: '1'
+    }, {
+      name: '使用及清零',
+      typeId: '2'
+    }],
+  },
+  handleTap: function(e) {
+    let id = e.currentTarget.id;
+    if(id){
+      this.setData({
+        currentId:id
+      })
+    }
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
